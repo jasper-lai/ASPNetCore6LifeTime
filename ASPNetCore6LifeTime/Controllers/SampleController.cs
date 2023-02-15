f21190898f3f6d984cbb29f60642ac3c9c19d9ef
jasper.lai <jasper.lai@gmail.com>
2023-02-15 15:37:18 +0800

    在 SampleController 加入含有傳入各介面作為參數的建構子

--- ASPNetCore6LifeTime/Controllers/SampleController.cs
+++ ASPNetCore6LifeTime/Controllers/SampleController.cs
@@ -1,9 +1,35 @@
 ﻿namespace ASPNetCore6LifeTime.Controllers
 {
+    using ASPNetCore6LifeTime.Interfaces;
     using Microsoft.AspNetCore.Mvc;
 
     public class SampleController : Controller
     {
+        private readonly ILogger<SampleController> _logger;
+        private readonly ITransientService _tranService1;
+        private readonly ITransientService _tranService2;
+        private readonly IScopedService _scopedService1;
+        private readonly IScopedService _scopedService2;
+        private readonly ISingletonService _singletonService1;
+        private readonly ISingletonService _singletonService2;
+
+        public SampleController(ILogger<SampleController> logger,
+            ITransientService tranService1,
+            ITransientService tranService2,
+            IScopedService scopedService1,
+            IScopedService scopedService2,
+            ISingletonService singletonService1,
+            ISingletonService singletonService2)
+        {
+            _logger = logger;
+            _tranService1 = tranService1;
+            _tranService2 = tranService2;
+            _scopedService1 = scopedService1;
+            _scopedService2 = scopedService2;
+            _singletonService1 = singletonService1;
+            _singletonService2 = singletonService2;
+        }
+
         public IActionResult Index()
         {
             return View();
--- README.md
+++ README.md
@@ -155,13 +155,35 @@ builder.Services.AddSingleton<ISingletonService, SampleService>();
 var app = builder.Build();
 ```
 
+### 步驟_6: 在 SampleController 加入含有傳入各介面作為參數的建構子
+在 SampleController 加入含有傳入各介面作為參數的建構子.  
 
+```csharp
+private readonly ILogger<SampleController> _logger;
+private readonly ITransientService _tranService1;
+private readonly ITransientService _tranService2;
+private readonly IScopedService _scopedService1;
+private readonly IScopedService _scopedService2;
+private readonly ISingletonService _singletonService1;
+private readonly ISingletonService _singletonService2;
 
-
-
-
-
-
+public SampleController(ILogger<SampleController> logger,
+    ITransientService tranService1,
+    ITransientService tranService2,
+    IScopedService scopedService1,
+    IScopedService scopedService2,
+    ISingletonService singletonService1,
+    ISingletonService singletonService2)
+{
+    _logger = logger;
+    _tranService1 = tranService1;
+    _tranService2 = tranService2;
+    _scopedService1 = scopedService1;
+    _scopedService2 = scopedService2;
+    _singletonService1 = singletonService1;
+    _singletonService2 = singletonService2;
+}
+```
 
 
 
