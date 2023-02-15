674154a55f7ed5f351e19429059fe0e4e67e3b59
jasper.lai <jasper.lai@gmail.com>
2023-02-15 15:24:58 +0800

    加入 SampleController 及對應的 View (Index.cshtml) : 建立 View 的過程中, 會加入 Microsoft.VisualStudio.Web.CodeGeneration.Design 6.0.11 的套件

---
+++ ASPNetCore6LifeTime/Controllers/SampleController.cs
@@ -0,0 +1,12 @@
+﻿namespace ASPNetCore6LifeTime.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class SampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
